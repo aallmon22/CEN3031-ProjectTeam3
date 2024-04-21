cc90159b6cd05a43d01022d573882bafe959a832
aallmon22 <68490614+aallmon22@users.noreply.github.com>
2024-04-21 14:22:48 -0400

    Update ElectedRepresentatives.js
    This links all the website links to the button - referenced from codeofordinances

--- src/ElectedRepresentatives.js
+++ src/ElectedRepresentatives.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import './ElectedRepresentatives.css';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import complianceUrls from "./ElecdRepURLs.json"; // Ensure the path is correct
 
 function ElectedRepresentatives() {
-    let { cityName } = useParams();
+  const [urls, setUrls] = useState({ ElectedRepresentatives: ""});
+  const { cityName } = useParams();
 
-    return (
-        <div className="representative-container">
-            <h1>{cityName} Elected Representatives</h1>
+  useEffect(() => {
+    const cityData = complianceUrls[cityName] || {
+      ElectedRepresentatives: "",
+    };
+    setUrls(cityData);
+  }, [cityName]);
+
+  return (
+    <div>
+      <h1>{cityName} Government Officials</h1>
+      {urls.ElectedRepresentatives && (
+        <div>
+          <a
+            href={urls.ElectedRepresentatives}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Elected Representatives
+          </a>
         </div>
-    );
+      )}
+      
+    </div>
+  );
 }
+
 export default ElectedRepresentatives;
